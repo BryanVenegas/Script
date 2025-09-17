7de246d5df2a203c42835727e2f30ab4f6f723b7
BryanVenegas <20243rd060@utez.edu.mx>
2025-09-17 16:03:13 -0600

    se agrega funcion para ver los repositorios

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ git_dir = r"C:\Users\veneg\Downloads\Programacion"
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir, name))and 
          os.path.exists(os.path.join(git_dir, name, ".git"))]
+selected_repo = None
+if repos:
+    print("Repositorios encontrados:")
+    for idx, repo in enumerate(repos,1):
+        print(f"{idx}. {repo}")
+        
+else:
+    print("No se encontraron repositorios Git en el directorio especificado.")
